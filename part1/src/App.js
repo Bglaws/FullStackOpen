9b665f164f6c068faea44024c5ea17c47b6d3a55
Bglaws <glaws.brian@gmail.com>
2021-09-19 16:51:45 -0400

    exercise 1 test

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react'
 
+const part1 = () => {
+  const exercises1 = 10
+  return (
+    <div>
+      <p>Fundamentals of React</p>
+      <p>Number of exercises {exercises1}</p>
+    </div>
+  )
+}
+const part2 = () => {
+  const exercises2 = 7
+  return (
+    <div>
+      <p>Using props to pass data</p>
+      <p>Number of exercises {exercises2}</p>
+    </div>
+  )
+}
+const part3 = () => {
+  const exercises3 = 14
+  return (
+    <div>
+      <p>State of a component</p>
+      <p>Number of exercises {exercises3}</p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <part1 />
+      <part2 />
+      <part3 />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
