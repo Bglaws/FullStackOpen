c5b3e96cf7209eea6c164741ca60331f01c87bc0
Brian Glaws <glaws.brian@gmail.com>
2021-09-27 21:16:02 -0400

    javascript exercises

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,43 +1,39 @@
 import React from 'react'
 
-const part1 = () => {
-  const exercises1 = 10
-  return (
-    <div>
-      <p>Fundamentals of React</p>
-      <p>Number of exercises {exercises1}</p>
-    </div>
-  )
-}
-const part2 = () => {
-  const exercises2 = 7
-  return (
-    <div>
-      <p>Using props to pass data</p>
-      <p>Number of exercises {exercises2}</p>
-    </div>
-  )
-}
-const part3 = () => {
-  const exercises3 = 14
-  return (
-    <div>
-      <p>State of a component</p>
-      <p>Number of exercises {exercises3}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
-
+  
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <h1>{course}</h1>
-      <part1 />
-      <part2 />
-      <part3 />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <h1>{course.name}</h1>
+      <p>
+        {course.parts[0].name}, Number of exercises {course.parts[0].exercises}
+      </p>
+      <p>
+        {course.parts[1].name}, Number of exercises {course.parts[1].exercises}
+      </p>
+      <p>
+        {course.parts[2].name}, Number of exercises {course.parts[2].exercises}
+      </p>
+      <p> Total number of exercises:  
+        {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}
+      </p>
     </div>
   )
 }
