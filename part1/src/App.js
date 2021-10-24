c909c0c9e4d00ab6c2f5f83b62a7b10c0a3e9c4a
Bglaws <glaws.brian@gmail.com>
2021-10-24 14:00:10 -0400

    exercise 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -4,19 +4,20 @@ const App = () => {
   // save clicks of each button to its own state
 
   const [clicks, setClicks] = useState ({
-    good: 0, bad: 0, neutral: 0
+    good: 0, bad: 0, neutral: 0, total: 0
   })
 
   const handleGood = () => {
-    setClicks({...clicks, good: clicks.good + 1})
+    setClicks({...clicks, good: clicks.good + 1, total: clicks.total + 1})
+    
   }
 
   const handleNeutral = () => {
-    setClicks({...clicks, neutral: clicks.neutral + 1})
+    setClicks({...clicks, neutral: clicks.neutral + 1, total: clicks.total + 1})
   }
 
   const handleBad = () => {
-    setClicks({...clicks, bad: clicks.bad + 1})
+    setClicks({...clicks, bad: clicks.bad + 1, total: clicks.total + 1})
   }
 
   return (
@@ -36,6 +37,7 @@ const App = () => {
         <p>good: {clicks.good}</p>
         <p>neutral: {clicks.neutral}</p>
         <p>bad: {clicks.bad}</p>
+        <p>total: {clicks.total}</p>
       </div>
     </div>
   )
